e1ad3e47532343817b484178c2d6ac38a9336bfb
Joe Torres <joetorres@Joes-MacBook-Pro.local>
2023-04-15 04:11:21 -0400

    Sorting input into lists

--- coachella.py
+++ coachella.py
@@ -1,16 +1,26 @@
 #Import csv files
 import pandas as pd
-BBHot = pd.read_csv('billboard_hot_100.csv', sep = ",")
-
-#print("\nInput CSV file = \n", BBHot)
+coachlineup = pd.read_csv('CoachLineupD1.csv', sep = ",")
 
 #Asks user for genre input
-usergenre = input("\nPlease enter your preferred genre of music (all lowercase).\n")
+print("\nPlease enter your preferred genre of music from this list below:")
+usergenre = input("hip hop, pop, rap, edm, indie, country, rock, r&b.\n")
+
 
 #Sorts and prints the billboard file to meet genre requirements
-sortbyGenre = BBHot[BBHot["Genres"]==usergenre]
+sortbyGenre = coachlineup[coachlineup["Genre"]==usergenre]
 print("\nSorted CSV by genre = \n", sortbyGenre)
 
-#Sorts and prints the names of the genre sorted list by name
-sortbyName = sortbyGenre.loc[:,"Artist Name(s)"]
-print("\nSorted CSV by name from genre sort = \n", sortbyName)
+#Separates the data into lists
+listNames = sortbyGenre['Artist'].tolist()
+listStartTime = sortbyGenre['Start'].tolist()
+listEndTime = sortbyGenre['End'].tolist()
+
+#Reverses the order of the lists to fix time error
+finlistNames = listNames.reverse()
+finlistStartTime = listStartTime.reverse()
+finlistEndTime = listEndTime.reverse()
+
+print("\n", listNames)
+print("\n", listStartTime)
+print("\n", listEndTime)
